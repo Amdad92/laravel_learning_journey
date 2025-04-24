bd97dc0c37812ee4bb184755e92d06e9f226dc25
Amdad92 <emdadctg92@gmail.com>
2025-04-25 00:24:07 +0600

    feat: Add basic route examples with parameter validation
    - Implement greeting route as basic example
    - Add numeric ID parameter route with whereNumber()
    - Create alpha username validation using whereAlpha()
    - Include language selection validation with whereIn()
    - Add regex validation for lowercase names
    - Demonstrate combined parameter validation

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Basic route
-
-// Responds to GET /greeting
-
+// Basic route
 Route::get('/greeting', function () {
     return 'Hello World from Web Routes';
 });
 
-
-// Route parameter
-
-//Basic parameter:
-
+// Route parameters
 Route::get('/user/{id}', function ($id) {
     return "User ID: $id";
 });
 
-//Multiple Parameters:
-
 Route::get('/post/{postId}/comment/{commentId}', function ($postId, $commentId) {
     return "Post: $postId, Comment: $commentId";
 });
+
+// Number validation
+Route::get('product/{id}', function (string $id) {
+    return "Works!! $id";
+})->whereNumber('id');
+
+// Alpha validation
+Route::get('user/{username}', function (string $username) {
+    return "Works!! $username";
+})->whereAlpha('username');
+
+// Language selection validation
+Route::get('{lang}/product', function (string $lang) {
+    return "Your selected language is $lang";
+})->whereIn('lang', ['English', 'Bengali']);
+
+// Regex validation
+Route::get('users/{name}', function (string $name) {
+    return "Name accepted $name";
+})->where('name', '[a-z]+');
+
+// Combined validation
+Route::get('{lang}/product/{id}', function (string $lang, string $id) {
+    return "Your selected language is $lang and id is $id";
+})->where([
+    'lang' => '[a-z]{2}',
+    'id' => '\d{4,}'
+]);
