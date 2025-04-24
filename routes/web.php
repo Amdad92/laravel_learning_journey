2283060ef142f9ddbd438ca57dda27560bf0f4a6
Amdad92 <emdadctg92@gmail.com>
2025-04-25 00:17:58 +0600

    feat: Add basic route examples with parameter validation
    - Implement greeting route as basic example
    - Add numeric ID parameter route with whereNumber()
    - Create alpha username validation using whereAlpha()
    - Include language selection validation with whereIn()
    - Add regex validation for lowercase names
    - Demonstrate combined parameter validation

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//Basic route
 
-Route::get('/', function () {
-    return view('welcome');
+// Responds to GET /greeting
+
+Route::get('/greeting', function () {
+    return 'Hello World from Web Routes';
+});
+
+
+// Route parameter
+
+//Basic parameter:
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: $id";
+});
+
+//Multiple Parameters:
+
+Route::get('/post/{postId}/comment/{commentId}', function ($postId, $commentId) {
+    return "Post: $postId, Comment: $commentId";
 });
